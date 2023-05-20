f87afd6233f86fe0e4198a096450c87f7cd0f52c
Vigneshwaran-Duraisamy <vijayvignesh561@gmail.com>
2023-05-20 16:40:59 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import FormOne from './Components/FormOne';
+import 'typeface-nunito';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import FormTwo from './Components/FormTwo';
+import SubForm from './Components/SubForm';
+import FormThree from './Components/FormThree';
+import FormFour from './Components/FormFour';
+import SuccessPage from './Components/SuccessPage';
+import ErrorPage from './Components/ErrorPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app' >
+      <Routes>
+        <Route path='/' element={<FormOne />} />
+        <Route path='/step-two' element={<FormTwo/>} />
+        <Route path='/step-two/sub-form' element={<SubForm/>} />
+        <Route path='/step-two/sub-form/step-three' element={<FormThree/>} />
+        <Route path='/step-two/sub-form/step-three/step-four' element={<FormFour/>} />
+        <Route path='/step-two/sub-form/step-three/step-four/step-success' element={<SuccessPage/>} />
+        <Route path='/err' element={<ErrorPage/>} />
+      </Routes>
+      
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,120 @@
+export const childSymptomsList = [
+    {
+        id: 'childSymptomsList01',
+        icon: "fa-regular:comments",
+        activeIcon: "symptoms07",
+        name: "Speech and Communication",
+        info: "Difficulty in speaking, stutters, stammers..."
+    },
+    {
+        id: 'childSymptomsList02',
+        icon: "lucide:apple",
+        activeIcon: "symptoms08",
+        name: "Food and Nutrition",
+        info: "Underweight child, food allergies, picky eater..."
+    },
+    {
+        id: 'childSymptomsList03',
+        icon: "bi:graph-up-arrow",
+        activeIcon: "symptoms09",
+        name: "Developmental Issues",
+        info: "Premature birth, ADHD, Autism..."
+    },
+    {
+        id: 'childSymptomsList04',
+        icon: "icomoon-free:plus",
+        activeIcon: "symptoms10",
+        name: "Skin, Allergies, Sleep etc.",
+        info: "Rashes, acne, distrubed sleep, dry skin..."
+    },
+    {
+        id: 'childSymptomsList05',
+        icon: "ph:person-simple-run-light",
+        activeIcon: "symptoms11",
+        name: "Gross Motor and Movement",
+        info: "Crawling, walking, jumping, catching..."
+    },
+    {
+        id: 'childSymptomsList06',
+        icon: "ph:baby-light",
+        activeIcon: "symptoms12",
+        name: "Behavior and Social",
+        info: "Cries a lot, tantrums, pinches, bites..."
+    }
+]
+
+
+export const childSymptomsType = [
+    {
+        symptoms: [
+            "Does not talk much",
+            "Speech is not clear",
+            "Does not understand requests",
+            "Hard to understand what they are saying",
+            "Stutters or stammers",
+            "Other issue"
+        ],
+        name: "Speech and Communication"
+    },
+    {
+        symptoms: [
+            "Child is underweight",
+            "Lactaction / milk supply",
+            "Starting solids",
+            "child is a picky eater",
+            "constipation /colic/ gas",
+            "Food allergies",
+            "Other issue"
+        ],
+        name: "Food and Nutrition"
+    },
+    {
+        symptoms: [
+            "Baby was born premature",
+            "Has autism",
+            "Has ADHD",
+            "Has Cerebral Palsy",
+            "Has Down Syndrome",
+            "Other issue"
+        ],
+        name: "Developmental Issues"
+    },
+    {
+        symptoms: [
+            "Dry skin/ eczema",
+            "Baby acne",
+            "Cradle cap",
+            "child has disturbed sleep",
+            "child does not sleep much",
+            "rash on skin",
+            "Other issue"
+        ],
+        name: "Skin, Allergies, Sleep etc."
+    },
+    {
+        symptoms: [
+            "Sitting up",
+            "Crawling",
+            "Walking",
+            "Running",
+            "Jumping/ Climbing",
+            "Catching/ throwing",
+            "Other issue"
+        ],
+        name: "Gross Motor and Movement"
+    },
+    {
+        symptoms: [
+            "Does not smile or make eye contact",
+            "Play is different vs other kids",
+            "Lacks interest in other people",
+            "Throws tantrums",
+            "Cries a lot",
+            "Hits/ pinches/ bites",
+            "Other issue"
+        ],
+        name: "Behavior and Social"
+    }
+]
+
+export const placeHolderContent = "Mention any medical history, family history, any incident which caused concern, food and sleep routine" 
